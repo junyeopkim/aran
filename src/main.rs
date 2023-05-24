9df41c7e735e5a48ca12ae5d25f83235bf6ae96c
Junyeop Kim <junyeopkim@gmail.com>
2023-05-24 22:38:40 +0900

    actix application part tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use std::sync::Mutex;
+
+struct AppStateWithCounter {
+    counter: Mutex<i32>,
+}
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -14,15 +19,31 @@ async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
+async fn index(data: web::Data<AppStateWithCounter>) -> impl Responder {
+    let mut counter = data.counter.lock().unwrap();
+    *counter += 1;
+
+    format!("Request number: {counter}")
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let counter = web::Data::new(AppStateWithCounter {
+        counter: Mutex::new(0),
+    });
+
+    HttpServer::new(move || {
         App::new()
             .service(hello)
             .service(echo)
             .route("/hey", web::get().to(manual_hello))
+            .service(
+                web::scope("/app")
+                    .app_data(counter.clone())
+                    .route("/index.html", web::get().to(index))
+            )
     })
-    .bind(("0.0.0.0", 8080))?
+    .bind(("127.0.0.1", 8080))?
     .run()
     .await
 }
